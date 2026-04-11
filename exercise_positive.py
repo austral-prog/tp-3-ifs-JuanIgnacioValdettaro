4c1dcc3f3cf8d46502cf262a46df2dca057459a6
JuanIgnacioValdettaro <jivaldettaro@mail.austral.edu.ar>
2026-04-11 19:56:18 -0300

    Add files via upload

--- exercise_positive.py
+++ exercise_positive.py
@@ -1,20 +1,20 @@
 def positive():
     """
-       Ejercicio 1 - Clasificar Número
+    Ejercicio 1 - Clasificar Número
 
-       Leer un número entero mediante input(). Determinar si es positivo, negativo o cero
-       e imprimir el resultado correspondiente.
+    Leer un número entero mediante input(). Determinar si es positivo, negativo o cero
+    e imprimir el resultado correspondiente.
 
-       Ejemplo:
-           Para la entrada "5", la salida esperada es:
-           El numero es positivo
+    Ejemplo:
+        Para la entrada "5", la salida esperada es:
+        El numero es positivo
 
-           Para la entrada "-3", la salida esperada es:
-           El numero es negativo
+        Para la entrada "-3", la salida esperada es:
+        El numero es negativo
 
-           Para la entrada "0", la salida esperada es:
-           El numero es cero
-       """
+        Para la entrada "0", la salida esperada es:
+        El numero es cero
+    """
     numero = int(input())
     if numero > 0:
         print("El numero es positivo")
